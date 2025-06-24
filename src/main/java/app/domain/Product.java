a0795b7fa88b4a8b47bc94e257dc4e2eea3d0a6f
Nastya1988-byte <belitskaya.a.dian@gmail.com>
2025-06-24 12:58:37 +0200

    init

---
+++ src/main/java/app/domain/Product.java
@@ -0,0 +1,68 @@
+package app.domain;
+
+import java.util.Objects;
+
+public class Product {
+    private final Long id;
+    private boolean isActive;
+    private String name;
+    private Double price;
+
+    public Product(Long id, boolean isActive, String name, Double price) {
+        this.id = id;
+        this.isActive = isActive;
+        this.name = name;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return isActive == product.isActive && Objects.equals(id, product.id) && Objects.equals(name, product.name) && Objects.equals(price, product.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, isActive, name, price);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Product{");
+        sb.append("id=").append(id);
+        sb.append(", isActive=").append(isActive);
+        sb.append(", name='").append(name).append('\'');
+        sb.append(", price=").append(price);
+        sb.append('}');
+        return sb.toString();
+    }
+}
